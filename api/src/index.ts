f3c939aac0e217279b11130def3fe7845e5fd55e
MakindeTobby <makinde.tobiloba@gmail.com>
2025-10-15 15:22:28 +0100

    setup boilerplates

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import productRoutes from "./routes/products/index";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -6,6 +7,8 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello Worlddd!");
 });
+
+app.use("/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
